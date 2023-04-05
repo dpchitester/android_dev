69740bfba3b4de1077b4e25a313532867d1f3118
Donald Chitester <dpchitester@gmail.com>
2023-04-04 22:24:57 -0600

    pybak

--- pybackup/gitops.py
+++ pybackup/gitops.py
@@ -1,47 +1,44 @@
 from opbase import OpBase
 from edge import Edge, findEdge
 
-def gitck1(Si, wt):
-    import asyncrun as ar
-    from statushash import ldh_f, ldhset, rdh_f, rdhset
-    from bhash import blakeHash
-    Dh1 = ldh_f(Si)
-    cmd = 'git status --porcelain --untracked-files=all'
-    rc = ar.run1(cmd, cwd=wt)
-    if rc == 0:
-        rv = ar.txt.rstrip()
-        Dh2 = blakeHash(rv)
-        if len(rv) == 0:
-            ldhset(Si, Dh2)
-        elif Dh2 != Dh1:
-            print(rv)
-        return (Dh2, len(rv) > 0 and Dh2 != Dh1)
-
-
-def gitck2(Si, wt):
-    import asyncrun as ar
-    from statushash import ldh_f, ldhset, rdh_f, rdhset
-    Dh1 = ldh_f(Si)
-    if Dh1 is None:
-        Dh1 = 0
-    cmd = 'git rev-list --count bitbucket/master..master'
-    rc = ar.run1(cmd, cwd=wt)
-    if rc == 0:
-        rv = ar.txt.rstrip()
-        Dh2 = int(rv)
-        if Dh2 == 0:
-            ldhset(Si, Dh2)
-        return (Dh2, Dh2 > Dh1)
-
 def gitcmd(cmd, wt):
+    import asyncrun as ar
     rc = ar.run1(cmd, cwd=wt)
     if rc != 0:
         raise Exception('gitcmd')
     return ar.txt.rstrip()
 
+def gitck1(Si, wt):
+    from statushash import ldh_f, ldhset, rdh_f, rdhset
+    from bhash import blakeHash
+    Dh1 = ldh_f(Si)
+    cmd = 'git status --porcelain --untracked-files=all'
+    rv = gitcmd(cmd, wt)
+    Dh2 = blakeHash(rv)
+    if len(rv) == 0:
+        ldhset(Si, Dh2)
+    elif Dh2 != Dh1:
+        print(rv)
+    return (Dh2, len(rv) > 0 and Dh2 != Dh1)
+
+
+def gitck2(Si, wt):
+    from statushash import ldh_f, ldhset, rdh_f, rdhset
+    Dh1 = ldh_f(Si)
+    if Dh1 is None:
+        Dh1 = 0
+    cmd = 'git rev-list --count bitbucket/master..master'
+    rv1 = gitcmd(cmd. wt)
+    cmd = 'git rev-list --count github/master..master'
+    rv2 = gitcmd(cmd. wt)
+    Dh2 = int(rv1) + int(rv2)
+    if Dh2 == 0:
+        ldhset(Si, Dh2)
+    return (Dh2, Dh2 > Dh1)
+
+
 
 def gitremoteck(Di, wt):
-    import asyncrun as ar
     from statushash import ldh_f, ldhset, rdh_f, rdhset
     Dh1 = rdh_f(Di)
     Dh2 = None
@@ -72,8 +69,8 @@ def gitremoteck(Di, wt):
         else:
             print('diverged')
             Dh2 = 4
-    except:
-        pass
+    except Exception as e:
+        print(e)
     if Dh2 is not None:
         if Dh2 == 1:
             rdhset(Di, Dh2)
