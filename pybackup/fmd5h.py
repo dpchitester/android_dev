37464f4eb74ce1e9246cf4f5b5a41566edcd7c4b
Donald Chitester <dpchitester@gmail.com>
2023-04-06 09:10:25 -0600

    pybak

--- pybackup/fmd5h.py
+++ pybackup/fmd5h.py
@@ -7,16 +7,12 @@ def md5sumf(Fn):
     if Fn.exists():
         #print('-md5sumf', Fn)
         ho = md5()
-        try:
-            with open(Fn, 'rb') as fh:
+        with open(Fn, 'rb') as fh:
+            b = fh.read(1 << 15)
+            while len(b) > 0:
+                ho.update(b)
                 b = fh.read(1 << 15)
-                while len(b) > 0:
-                    ho.update(b)
-                    b = fh.read(1 << 15)
-            return ho.digest()
-        except Exception as e:
-            print(e)
-            raise e
+        return ho.digest()
     return None
 
 
@@ -30,23 +26,23 @@ def finddict(d1, fp):
 
 
 def fmd5f(fp, sz, mt):
+    def new_hash():
+        nh = md5sumf(fp)
+        d1[fp.name] = (sz, mt, nh)
+        v.hf_dirty = True
+        v.hf_stm += 1
+        return nh
     d1 = finddict(v.fmd5hd, fp)
-    try:
-        (osz, omt, oh) = d1[fp.name]
-        v.hf_dh += 1
-    except KeyError:
-        (osz, omt, oh) = (-1, -1, b'')
+    if fp.name not in d1:
         v.hf_dm += 1
-        pp(fp, sz, mt, v.hf_dm)
-    if osz == sz and omt == mt:
-        v.hf_sth += 1
+        return new_hash()
     else:
-        try:
-            oh = md5sumf(fp)
-            d1[fp.name] = (sz, mt, oh)
-            v.hf_dirty = True
+        v.hf_dh += 1
+        (osz, omt, oh) = d1[fp.name]
+        if osz == sz and omt == mt:
+            v.hf_sth += 1
+            return oh
+        else:
             v.hf_stm += 1
-            pp(fp, sz, mt, oh, v.hf_stm)
-        except:
-            pass
-    return oh
+            return new_hash()
+
