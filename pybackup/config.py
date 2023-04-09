914b47c9e176139224b5aa4a3f34e6958dbf0c33
Donald Chitester <dpchitester@gmail.com>
2023-04-09 04:43:13 -0600

    pybak

--- pybackup/config.py
+++ pybackup/config.py
@@ -17,6 +17,84 @@ from mkzip import Mkzip
 from opbase import OpBase, OpBaseEncoder
 from statushash import ldhck, rdhck
 
+NodeTag: TypeAlias = str
+
+# any/all mostly local directory path(s)
+paths: Dict[NodeTag, Path] = {}
+
+# tag attributes/types/classes
+pres: Set[NodeTag] = set()
+pdirs: Set[NodeTag] = set()
+tdirs: Set[NodeTag] = set()
+srcs: Set[NodeTag] = set()
+tgts: Set[NodeTag] = set()
+codes: Set[NodeTag] = set()
+
+# checkers
+lckers: Dict[NodeTag, Callable] = {}
+rckers: Dict[NodeTag, Callable] = {}
+
+# operations (function objects)
+opdep: List[OpBase] = []
+
+# dependencies as edge set
+eDep: Set[Edge] = set()
+
+# dependencies as stored by di,si
+edges: Dict[Tuple[NodeTag, NodeTag], Edge] = {}
+
+Hash: TypeAlias = bytes
+
+Hdt1: TypeAlias = Dict[NodeTag, Hash]
+
+# directory lists hashes
+
+LDhd: Hdt1 = {}
+RDhd: Hdt1 = {}
+
+Hde: TypeAlias = Tuple[int, float, Hash]
+Hdt2: TypeAlias = Dict[Path, Union["Hdt2", Hde]]
+
+# local file md5 hashes
+fmd5hd: Hdt2 = {}
+
+# directory lists
+LDlls: Dict[NodeTag, List["DE"]] = {}
+RDlls: Dict[NodeTag, List["DE"]] = {}
+
+# update times of directory lists
+LDlls_xt: Dict[NodeTag, float] = {}
+RDlls_xt: Dict[NodeTag, float] = {}
+
+LDlls_changed: bool = False
+RDlls_changed: bool = False
+
+# pickle file filenames
+edgepf: Path | None = None
+ldllsf: Path | None = None
+rdllsf: Path | None = None
+fmd5hf: Path | None = None
+ldhpf: Path | None = None
+rdhpf: Path | None = None
+
+# worktree of git repo
+worktree: Path | None = None
+
+# directory list hashing stats
+hf_dirty: bool = False
+hf_dm: int = 0
+hf_dh: int = 0
+hf_pm: int = 0
+hf_ph: int = 0
+hf_stm: int = 0
+hf_sth: int = 0
+sfb: int = 0
+
+# directory list getting stats
+dl0_cs = 0
+dl1_cs = 0
+dl2_cs = 0
+dl3_cs = 0
 
 def initConfig():
     addPre("FLAGS", os.environ["HOME"])
@@ -101,7 +179,6 @@ def initConfig():
                 ".bashrc",
                 ".bashrc0",
                 ".profile",
-                ".config/rclone/*",
                 ".plaid-cli/*",
                 ".plaid-cli/data/*",
                 ".plaidrc",
@@ -134,11 +211,11 @@ def initConfig():
     addArc(op1)
 
     npl1 = ("bin", "bash")
-    op1 = LocalCopy(npl1, npl1, {"files": ["termux-*", "pbu", "rbu"], "exec": True})
+    op1 = LocalCopy(npl1, npl1, {"files": ["termux-*", "pbu", "rbu", "qe"], "exec": True})
     addArc(op1)
 
     npl1 = ("bash", "bin")
-    op1 = LocalCopy(npl1, npl1, {"files": ["termux-*", "pbu", "rbu"], "exec": False})
+    op1 = LocalCopy(npl1, npl1, {"files": ["termux-*", "pbu", "rbu", "qe"], "exec": False})
     addArc(op1)
 
     npl1 = ("sh", "bash")
@@ -339,79 +416,3 @@ class DE:
         )
 
 
-NodeTag: TypeAlias = str
-
-# any/all mostly local directory path(s)
-paths: Dict[NodeTag, Path] = {}
-
-# tag attributes/types/classes
-pres: Set[NodeTag] = set()
-pdirs: Set[NodeTag] = set()
-tdirs: Set[NodeTag] = set()
-srcs: Set[NodeTag] = set()
-tgts: Set[NodeTag] = set()
-codes: Set[NodeTag] = set()
-
-# checkers
-lckers: Dict[NodeTag, Callable] = {}
-rckers: Dict[NodeTag, Callable] = {}
-
-# operations (function objects)
-opdep: List[OpBase] = []
-
-# dependencies as edge set
-eDep: Set[Edge] = set()
-
-# dependencies as stored by di,si
-edges: Dict[Tuple[NodeTag, NodeTag], Edge] = {}
-
-Hash: TypeAlias = bytes
-
-Hdt1: TypeAlias = Dict[NodeTag, Hash]
-
-# directory lists hashes
-
-LDhd: Hdt1 = {}
-RDhd: Hdt1 = {}
-
-Hde: TypeAlias = Tuple[int, float, Hash]
-Hdt2: TypeAlias = Dict[Path, Union["Hdt2", Hde]]
-
-# local file md5 hashes
-fmd5hd: Hdt2 = {}
-
-# directory lists
-LDlls: Dict[NodeTag, List[DE]] = {}
-RDlls: Dict[NodeTag, List[DE]] = {}
-
-# update times of directory lists
-LDlls_xt: Dict[NodeTag, float] = {}
-RDlls_xt: Dict[NodeTag, float] = {}
-
-LDlls_changed: bool = False
-RDlls_changed: bool = False
-
-# pickle file filenames
-edgepf: Path | None = None
-ldllsf: Path | None = None
-rdllsf: Path | None = None
-fmd5hf: Path | None = None
-ldhpf: Path | None = None
-rdhpf: Path | None = None
-
-# worktree of git repo
-worktree: Path | None = None
-
-# directory list hashing stats
-hf_dirty: bool = False
-hf_dm: int = 0
-hf_dh: int = 0
-hf_pm: int = 0
-hf_ph: int = 0
-hf_stm: int = 0
-hf_sth: int = 0
-sfb: int = 0
-dl0_cs = 0
-dl1_cs = 0
-dl2_cs = 0
-dl3_cs = 0
