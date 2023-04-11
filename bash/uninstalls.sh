7f826302e0b5f1b7aff360eee41abec05f6d48ab
Donald Chitester <dpchitester@gmail.com>
2023-04-11 05:57:12 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -2,6 +2,7 @@
 
 
 # pkg uninstall apt
+# pkg uninstall attr
 # pkg uninstall autoconf
 # pkg uninstall automake
 # pkg uninstall bash-completion
@@ -10,7 +11,7 @@
 # pkg uninstall binutils-bin
 # pkg uninstall binutils-libs
 # pkg uninstall binutils
-# pkg uninstall bison
+pkg uninstall bison
 # pkg uninstall brotli
 # pkg uninstall build-essential
 # pkg uninstall busybox
@@ -22,7 +23,7 @@
 # pkg uninstall command-not-found
 # pkg uninstall coreutils
 # pkg uninstall curl
-# pkg uninstall dart
+pkg uninstall dart
 # pkg uninstall dash
 # pkg uninstall debianutils
 # pkg uninstall dialog
@@ -131,7 +132,7 @@
 # pkg uninstall patch
 # pkg uninstall pcre2
 # pkg uninstall pcre
-# pkg uninstall perl
+pkg uninstall perl
 # pkg uninstall pkg-config
 # pkg uninstall procps
 # pkg uninstall psmisc
@@ -162,21 +163,25 @@
 # pkg uninstall xz-utils
 # pkg uninstall zlib
 # pkg uninstall zstd
-# pip uninstall --------------- -------
-# pip uninstall asttokens       2.2.1
-# pip uninstall async-to-sync   0.2.3
-# pip uninstall black           23.3.0
-# pip uninstall cheap-repr      0.5.1
-# pip uninstall click           8.1.3
-# pip uninstall executing       1.2.0
-# pip uninstall isort           5.12.0
-# pip uninstall mypy-extensions 1.0.0
-# pip uninstall packaging       23.0
-# pip uninstall pathspec        0.11.1
-# pip uninstall pip             23.0.1
-# pip uninstall platformdirs    3.2.0
-# pip uninstall Pygments        2.14.0
-# pip uninstall setuptools      67.6.1
-# pip uninstall six             1.16.0
-# pip uninstall snoop           0.4.3
-# pip uninstall tarjan          0.2.3.2
+# pip uninstall ----------------- -------
+# pip uninstall asttokens         2.2.1
+# pip uninstall async-to-sync     0.2.3
+# pip uninstall black             23.3.0
+# pip uninstall cheap-repr        0.5.1
+# pip uninstall click             8.1.3
+# pip uninstall executing         1.2.0
+# pip uninstall isort             5.12.0
+# pip uninstall mypy              1.2.0
+# pip uninstall mypy-extensions   1.0.0
+# pip uninstall packaging         23.0
+# pip uninstall pathspec          0.11.1
+# pip uninstall pip               23.0.1
+# pip uninstall platformdirs      3.2.0
+# pip uninstall Pygments          2.14.0
+# pip uninstall pytoolconfig      1.2.5
+# pip uninstall rope              1.7.0
+# pip uninstall setuptools        67.6.1
+# pip uninstall six               1.16.0
+# pip uninstall snoop             0.4.3
+# pip uninstall tarjan            0.2.3.2
+# pip uninstall typing_extensions 4.5.0
