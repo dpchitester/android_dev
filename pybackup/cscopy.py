3a02343695aafd934ba191462cc8bf1779bc432d
Donald Chitester <dpchitester@gmail.com>
2023-04-15 06:24:25 -0600

    pybak

--- pybackup/cscopy.py
+++ pybackup/cscopy.py
@@ -198,7 +198,7 @@ class CSCopy(OpBase):
             if bv.sfc.fc == 0:
                 bv.do_copying()
             if bv.sfc.fc == 0:
-                if "delete" in self.opts and self.opts["delete"]:
+                if "delete" in self.opts and self.opts["delete"] and len(self.f2d):
                     bv.do_deletions()
             if bv.ac2:
                 pass
