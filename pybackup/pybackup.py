a55b5b4634dc42c39384f31d180cfc184eac8917
Donald Chitester <dpchitester@gmail.com>
2023-04-16 22:40:19 -0600

    pybak

--- pybackup/pybackup.py
+++ pybackup/pybackup.py
@@ -46,66 +46,66 @@ def wsetup():
 
 def cb1():
     global th2, in1, sis
-    print("-cb1-1")
+    # print("-cb1-1")
     for ev in in1:
-        print("-cb1-4 ev:", ev)
+        # print("-cb1-4 ev:", ev)
         si = wdsi[ev.watch]
-        print("-cb1-5 si:", si)
+        # print("-cb1-5 si:", si)
         p = ev.path
-        print("-cb1-6 p:", p)
+        # print("-cb1-6 p:", p)
         if not ev.mask & Mask.ISDIR:
             rfn = str(p.relative_to(v.src(si)))
-            print("-cb1-7 rfn:", rfn)
+            # print("-cb1-7 rfn:", rfn)
             if si not in sis:
-                print("-cb1-8 []", [])
+                # print("-cb1-8 []", [])
                 sis[si] = []
             if rfn not in sis[si]:
-                print("-cb1-9 rfn:", rfn)
+                # print("-cb1-9 rfn:", rfn)
                 sis[si].append(rfn)
                 if th2 is None:
-                    print("-cb1-10 th2 is None")
+                    # print("-cb1-10 th2 is None")
                     th2 = th.Thread(target=proc_events)
                     th2.start()
                 elif not th2.is_alive():
-                    print("-cb1-11 th2 not is_alive")
+                    # print("-cb1-11 th2 not is_alive")
                     th2 = th.Thread(target=proc_events)
                     th2.start()
-    print("-cb1-13")
+    # print("-cb1-13")
 
 
 def proc_events():
     global sis
-    print(sis)
+    # print(sis)
     tsis, sis = sis, {}
     for si in tsis:
         p = v.src(si)
         fl = tsis[si]
         if len(fl):
-            print("-proc_events-3: updateDEs", p, fl)
+            # print("-proc_events-3: updateDEs", p, fl)
             updateDEs(p, fl)
 
 
 def rt2():
     global th1
-    print("-rt2-1")
+    # print("-rt2-1")
     itc = 0
     while True:
         itc += 1
-        print("-rt2-2")
+        # print("-rt2-2")
         if not th1 or not th1.is_alive():
             th1 = th.Thread(target=cb1)
             th1.start()
         cl = clean()
         if cl:
-            print("-rt2-3")
+            # print("-rt2-3")
             print("no backups appear pending")
             rv1 = False
             break
         else:
-            print("-rt2-4")
+            # print("-rt2-4")
             print("backups appear pending")
             rv1 = opExec()
-        print("-rt2-5")
+        # print("-rt2-5")
 
 
 def main():
