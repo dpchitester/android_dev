3460b82246e0c21cc9f069aafe0ac60f202b7372
Donald Chitester <dpchitester@gmail.com>
2023-04-24 23:28:09 -0600

    pybak

--- pybackup/sd.py
+++ pybackup/sd.py
@@ -158,18 +158,18 @@ class FS_Mixin(SD):
         else:
             ch = "l"
         if self.Dll is None or (self.isremote and self.Dlls_xt + rto1 <= time.time()):
-            print("sucking/scanning for", self.tag, ch + "dll...", end="")
+            # print("sucking/scanning for", self.tag, ch + "dll...", end="")
             rv = self.getdll()
             if rv is not None:
-                print("done.")
+                # print("done.")
                 self.Dll = rv
                 self.Dlls_xt = time.time()
                 self.Dlls_changed = True
             else:
-                print("failed.")
+                # print("failed.")
                 pass
         else:
-            print("fetched", self.tag, ch + "dll from cache.")
+            # print("fetched", self.tag, ch + "dll from cache.")
             pass
         return self.Dll
 
