5c256c672844bf95b296ce860f85024a456117dd
Donald Chitester <dpchitester@gmail.com>
2023-04-26 11:32:27 -0600

    pybak

--- pybackup/pybackup.py
+++ pybackup/pybackup.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
         yappi.get_thread_stats().sort("id","asc").print_all(out=fh)
         yappi.get_func_stats().sort("subtime","desc").print_all(out=fh,
             columns={
-                0: ("ncall", 8+12),
+                0: ("ncall", 8+5),
                 1: ("tsub", 8+3),
                 2: ("ttot", 8+3),
                 3: ("tavg", 8+3),
