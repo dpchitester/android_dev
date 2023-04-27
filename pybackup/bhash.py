e23f6637d6c0ee0f4e7a82188262273bf76c3827
Donald Chitester <dpchitester@gmail.com>
2023-04-26 23:20:55 -0600

    pybak

--- pybackup/bhash.py
+++ pybackup/bhash.py
@@ -37,6 +37,31 @@ def bhu(ho, it1):
     except KeyError:
         print("key error", type(it1), "??")
 
+def bhu2(ho, it):
+    match type(it):
+        case 'bytes':
+            ho.update(it)
+        case 'int':
+            ho.update(pack("i", it))
+        case 'float':
+            ho.update(pack("f", it))
+        case 'str':
+            ho.update(it.encode())
+        case 'Path' | 'PosixPath' | 'SD' | 'Ext3' | 'Fat32' | 'CS':
+            ho.update(bytes(it))
+        case 'tuple' | 'set' | 'list':
+            for it2 in it:
+                bhu(ho, it2)
+        case 'DE':
+            ho.update(bytes(it.nm))
+            ho.update(pack("i", it.i.sz))
+            ho.update(pack("f", it.i.mt))
+        case 'FSe':
+            ho.update(pack("i", it.sz))
+            ho.update(pack("f", it.mt))
+        case _:
+            print("key error", type(it), "??")
+
 
 # flattened list blake2b with integer result
 def blakeHash(it):
