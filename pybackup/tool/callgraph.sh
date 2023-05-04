058f7dfa3f094aa644730d427964716f824fc9e6
Donald Chitester <dpchitester@gmail.com>
2023-05-03 21:53:44 -0600

    pybak

--- pybackup/tool/callgraph.sh
+++ pybackup/tool/callgraph.sh
@@ -2,6 +2,6 @@ clear
 rm pybackup.svg
 python -m pybackup
 gprof2dot -z pybackup:129:main -f pstats -o pybackup.gv pybackup.pstat
-rm pybackup.pstats
-dot -Tsvg -Ksfdp -o pybackup.svg pybackup.gv
+rm pybackup.pstat
+dot -Tsvg -o pybackup.svg pybackup.gv
 rm pybackup.gv
