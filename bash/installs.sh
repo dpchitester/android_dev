93678c1c5e00b8b7b8bac55cf20dcbfa59af2729
Donald Chitester <dpchitester@gmail.com>
2023-05-04 16:10:01 -0600

    pybak

--- bash/installs.sh
+++ bash/installs.sh
@@ -4,6 +4,7 @@
 # pkg install apt
 # pkg install bash-completion
 # pkg install bash
+# pkg install brotli
 # pkg install busybox
 # pkg install bzip2
 # pkg install ca-certificates
@@ -19,49 +20,81 @@
 # pkg install dpkg
 # pkg install ed
 # pkg install findutils
+# pkg install fontconfig
+# pkg install freetype
+# pkg install fribidi
 # pkg install gawk
 # pkg install gdbm
+# pkg install gdk-pixbuf
+# pkg install giflib
 # pkg install git-crypt
 # pkg install git
 # pkg install glib
 # pkg install gpgv
+# pkg install graphviz
 # pkg install grep
 # pkg install gzip
+# pkg install harfbuzz
 # pkg install inetutils
 # pkg install inotify-tools
 # pkg install less
 # pkg install libandroid-glob
 # pkg install libandroid-posix-semaphore
+# pkg install libandroid-shmem
 # pkg install libandroid-support
+# pkg install libaom
 # pkg install libassuan
 # pkg install libbz2
 # pkg install libc++
+# pkg install libcairo
 # pkg install libcap-ng
 # pkg install libcompiler-rt
 # pkg install libcrypt
 # pkg install libcurl
+# pkg install libdav1d
+# pkg install libde265
 # pkg install libevent
 # pkg install libexpat
 # pkg install libffi
 # pkg install libgcrypt
+# pkg install libgd
 # pkg install libgmp
 # pkg install libgnutls
 # pkg install libgpg-error
+# pkg install libgraphite
+# pkg install libheif
 # pkg install libiconv
 # pkg install libidn2
+# pkg install libjpeg-turbo
 # pkg install libllvm
+# pkg install libltdl
 # pkg install liblz4
 # pkg install liblzma
+# pkg install liblzo
 # pkg install libmd
 # pkg install libmpfr
 # pkg install libnettle
 # pkg install libnghttp2
 # pkg install libnpth
+# pkg install libpixman
+# pkg install libpng
+# pkg install librav1e
+# pkg install librsvg
 # pkg install libsmartcols
 # pkg install libsqlite
 # pkg install libssh2
+# pkg install libtiff
 # pkg install libunistring
+# pkg install libwebp
+# pkg install libx11
+# pkg install libx265
+# pkg install libxau
+# pkg install libxcb
+# pkg install libxdmcp
+# pkg install libxext
+# pkg install libxft
 # pkg install libxml2
+# pkg install libxrender
 # pkg install lld
 # pkg install llvm
 # pkg install make
@@ -71,6 +104,7 @@
 # pkg install net-tools
 # pkg install openssl-1.1
 # pkg install openssl
+# pkg install pango
 # pkg install patch
 # pkg install pcre2
 # pkg install pkg-config
@@ -90,6 +124,7 @@
 # pkg install termux-keyring
 # pkg install termux-licenses
 # pkg install termux-tools
+# pkg install ttf-dejavu
 # pkg install unbound
 # pkg install unzip
 # pkg install util-linux
@@ -97,9 +132,18 @@
 # pkg install xz-utils
 # pkg install zlib
 # pkg install zstd
-# pip install ------------ -------
-# pip install asyncinotify 4.0.1
-# pip install pip          23.1.1
-# pip install setuptools   67.7.2
-# pip install tarjan       0.2.4
-# pip install xxhash       3.2.0
+# pip install --------------- -------
+# pip install asyncinotify    4.0.1
+# pip install black           23.3.0
+# pip install click           8.1.3
+# pip install Jinja2          3.1.2
+# pip install MarkupSafe      2.1.2
+# pip install mypy-extensions 1.0.0
+# pip install packaging       23.1
+# pip install pathspec        0.11.1
+# pip install pip             23.1.1
+# pip install platformdirs    3.5.0
+# pip install setuptools      67.7.2
+# pip install snakefood3      0.0.7
+# pip install tarjan          0.2.4
+# pip install xxhash          3.2.0
