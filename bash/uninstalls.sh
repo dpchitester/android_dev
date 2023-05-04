d52fcc6479aabf427a43de0c247c7173f8b0a5cb
Donald Chitester <dpchitester@gmail.com>
2023-05-04 09:51:38 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -3,42 +3,12 @@
 
 # pkg uninstall debianutils
 # pkg uninstall git-crypt
-pkg uninstall graphviz
 # pkg uninstall inotify-tools
-pkg uninstall lsof
-pkg uninstall nano
 # pkg uninstall openssl-1.1
-pkg uninstall pcre
 # pkg uninstall rclone
-# pip uninstall ----------------- ---------
-pip uninstall asttokens         2.2.1
-# pip uninstall asyncinotify      4.0.1
-pip uninstall black             23.3.0
-pip uninstall cheap-repr        0.5.1
-pip uninstall click             8.1.3
-pip uninstall executing         1.2.0
-pip uninstall gprof2dot         2022.7.29
-pip uninstall import-deps       0.2.0
-pip uninstall isort             5.12.0
-pip uninstall Jinja2            3.1.2
-pip uninstall MarkupSafe        2.1.2
-pip uninstall modulegraph2      2.2.1
-pip uninstall mypy-extensions   1.0.0
-pip uninstall objectgraph       1.0.3
-pip uninstall packaging         23.1
-pip uninstall pathspec          0.11.1
-# pip uninstall pip               23.1.1
-pip uninstall platformdirs      3.3.0
-pip uninstall pyAn              0.1.3
-pip uninstall pydeps            1.12.3
-pip uninstall Pygments          2.15.1
-pip uninstall pyinstrument      4.4.0
-# pip uninstall setuptools        67.7.2
-pip uninstall six               1.16.0
-pip uninstall snoop             0.4.3
-pip uninstall stdlib-list       0.8.0
-# pip uninstall tarjan            0.2.4
-pip uninstall tuna              0.5.11
-pip uninstall typing_extensions 4.5.0
-# pip uninstall xxhash            3.2.0
-pip uninstall yappi             1.4.0
+# pip uninstall ------------ -------
+# pip uninstall asyncinotify 4.0.1
+# pip uninstall pip          23.1.1
+# pip uninstall setuptools   67.7.2
+# pip uninstall tarjan       0.2.4
+# pip uninstall xxhash       3.2.0
