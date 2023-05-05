6c7c61c68a847beb7069d1d529171d338afe167e
Donald Chitester <dpchitester@gmail.com>
2023-05-04 21:56:10 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -3,22 +3,12 @@
 
 # pkg uninstall debianutils
 # pkg uninstall git-crypt
-# pkg uninstall graphviz
 # pkg uninstall inotify-tools
 # pkg uninstall openssl-1.1
 # pkg uninstall rclone
-# pip uninstall --------------- -------
-# pip uninstall asyncinotify    4.0.1
- pip uninstall black           23.3.0
- pip uninstall click           8.1.3
- pip uninstall Jinja2          3.1.2
- pip uninstall MarkupSafe      2.1.2
- pip uninstall mypy-extensions 1.0.0
- pip uninstall packaging       23.1
- pip uninstall pathspec        0.11.1
-# pip uninstall pip             23.1.1
- pip uninstall platformdirs    3.5.0
-# pip uninstall setuptools      67.7.2
- pip uninstall snakefood3      0.0.7
-# pip uninstall tarjan          0.2.4
-# pip uninstall xxhash          3.2.0
+# pip uninstall ------------ -------
+# pip uninstall asyncinotify 4.0.1
+# pip uninstall pip          23.1.1
+# pip uninstall setuptools   67.7.2
+# pip uninstall tarjan       0.2.4
+# pip uninstall xxhash       3.2.0
