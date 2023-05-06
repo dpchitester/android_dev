8fedfca4c6d9b1a29e810ba9655a5871c70b06a9
Donald Chitester <dpchitester@gmail.com>
2023-05-06 07:22:45 -0600

    pybak

--- pybackup/tool/rfmt2.py
+++ pybackup/tool/rfmt2.py
@@ -2,18 +2,18 @@
 from glob import glob
 from sys import exit
 
-from funcs import srun
-from gb_env import pdir
+#from funcs import srun
+#from gb_env import pdir
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
-from pygments.lexers import PrologLexer
+from pygments.lexers import PythonLexer
 
 print("-- rfmt2.py --")
 
 # cmd = 'yapf -i -r -vv ' + pdir['pyth'] + '/*.py'
 
-for fn in glob("/sdcard/projects/prolog/*.pl"):
+for fn in glob("/sdcard/projects/pybackup/*.py"):
     with open(fn, "r") as fv:
         code = fv.read()
         with open(fn + ".html", "w") as fv2:
-            highlight(code, PrologLexer(), HtmlFormatter(full=True), outfile=fv2)
+            highlight(code, PythonLexer(), HtmlFormatter(full=True), outfile=fv2)
