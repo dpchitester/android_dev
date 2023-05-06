db5170996e5b615e6ec44d6669ed9d717a5a6432
Donald Chitester <dpchitester@gmail.com>
2023-05-06 09:12:22 -0600

    pybak

--- pybackup/tool/mf.py
+++ pybackup/tool/mf.py
@@ -6,14 +6,6 @@ finder.run_script("pybackup.py")
 moduleslist = {}
 for name, mod in finder.modules.items():
     filename = mod.__file__
-    if filename is None:
-        continue
-    if "__" in filename:
-        continue
-    if 'python3.11' in filename:
-        continue
-    # if "python" in filename.lower():
-    #    continue
     moduleslist[name] = mod
     # print '%s: %s' % (name, filename)
     # print ','.join(mod.globalnames.keys()[:3])
