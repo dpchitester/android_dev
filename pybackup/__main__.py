a637e410a365b2870bed6fc49df3cda34468de2d
Donald Chitester <dpchitester@gmail.com>
2023-05-05 19:16:48 -0600

    pybak

--- pybackup/__main__.py
+++ pybackup/__main__.py
@@ -1,6 +1,6 @@
 import logging
 
-import pybackup
+from . import pybackup
 
 logging.basicConfig(filename="pybackup.log", encoding="utf-8", level=logging.DEBUG)
 logging.getLogger("asyncio").setLevel(logging.DEBUG)
