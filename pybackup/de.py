b42404278df5e92d692eb0875df61d51c1f023d1
Donald Chitester <dpchitester@gmail.com>
2023-05-06 22:35:17 -0600

    pybak

--- pybackup/de.py
+++ pybackup/de.py
@@ -2,13 +2,17 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
-@dataclass(order=True, unsafe_hash=True)
+@dataclass(order=True)
 class FSe:
     sz: int
     mt: float
+    def __hash__(self):
+        return hash((self.sz,self.mt))
 
 
-@dataclass(order=True, unsafe_hash=True)
+@dataclass(order=True)
 class DE:
     nm: Path
     i: FSe
+    def __hash__(self):
+        return hash((self.nm,self.i.sz,self.i.mt))
