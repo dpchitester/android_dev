f97bbf084d3a48c26b03dd9c7c85772a4c896760
Donald Chitester <dpchitester@gmail.com>
2023-05-13 15:38:06 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -7,14 +7,18 @@
 # pkg uninstall openssl-1.1
 # pkg uninstall rclone
 # pkg uninstall ruff
-# pip uninstall ----------------------
- pip uninstall antlr4-python3-runtime
+# pip uninstall -------------
 # pip uninstall asyncinotify
- pip uninstall hydra-core
- pip uninstall omegaconf
-pip uninstall packaging
+ pip uninstall configobj
+ pip uninstall dpath
+ pip uninstall parse-it
 # pip uninstall pip
+ pip uninstall pyhcl
+ pip uninstall python-dotenv
  pip uninstall PyYAML
+ pip uninstall six
 # pip uninstall tarjan
+ pip uninstall toml
+ pip uninstall xmltodict
 # pip uninstall xxhash
 # pip uninstall yappi
