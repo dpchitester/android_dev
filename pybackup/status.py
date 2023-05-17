e9e6e9589cc895d181553bc37acd5f9c01f6f5b9
Donald Chitester <dpchitester@gmail.com>
2023-05-17 10:25:51 -0600

    pybak

--- pybackup/status.py
+++ pybackup/status.py
@@ -66,7 +66,7 @@ def src_statuses2():
 
 def updatets(N):
     print("Status", N)
-    Sl = src_statuses2()
+    Sl = src_statuses()
     if len(Sl):
         print("changed: ", end="")
         for Si, Dh in Sl:
