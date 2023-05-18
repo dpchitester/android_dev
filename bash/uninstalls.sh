f77e669074619718d3b94f65f2726fcec0691986
Donald Chitester <dpchitester@gmail.com>
2023-05-17 20:53:21 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -1,21 +1,29 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 
-# pkg uninstall debianutils
 # pkg uninstall git-crypt
 # pkg uninstall inotify-tools
 # pkg uninstall openssl-1.1
 # pkg uninstall rclone
 # pkg uninstall ruff
+# pkg uninstall which
 # pip uninstall ---------------
-# pip uninstall asyncinotify
+# pip uninstall asttokens
 # pip uninstall black
+# pip uninstall cheap-repr
 # pip uninstall click
+# pip uninstall executing
 # pip uninstall mypy-extensions
 # pip uninstall packaging
 # pip uninstall pathspec
 # pip uninstall pip
 # pip uninstall platformdirs
+# pip uninstall promisio
+# pip uninstall Pygments
+# pip uninstall six
+# pip uninstall snakeviz
+# pip uninstall snoop
 # pip uninstall tarjan
+# pip uninstall tornado
 # pip uninstall xxhash
 # pip uninstall yappi
