be8ad4cb2335101d963f1e2397845b3df40f237d
Donald Chitester <dpchitester@gmail.com>
2023-05-28 03:32:19 -0600

    pybak

--- blog/blog.js
+++ blog/blog.js
@@ -273,7 +273,7 @@ async function lts() {
     return res.rows.item(uc).ts;
 }
 function maxdl_calc() {
-    let cashd = dl(4, 28);
+    let cashd = dl(5, 28);
     let dxd = dl(5, 3);
     maxdl.te.SetText(ncs(Math.max(cashd, dxd)));
     maxdl.num = ncs(Math.max(cashd, dxd));
