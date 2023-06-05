66a21d8887ba03606d4c7390cdb16978519c4f0c
Donald Chitester <dpchitester@gmail.com>
2023-06-05 09:31:26 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -2,12 +2,18 @@
 
 
 # pkg uninstall git-crypt
+# pkg uninstall nodejs
 # pkg uninstall openssl-1.1
 # pkg uninstall rclone
 # pkg uninstall ruff
 # pip uninstall ----------
+# pip uninstall esprima
+# pip uninstall Js2Py
 # pip uninstall pip
+# pip uninstall pyjsparser
 # pip uninstall setuptools
+# pip uninstall six
 # pip uninstall tarjan
+# pip uninstall tzlocal
 # pip uninstall xxhash
 # pip uninstall zipp
