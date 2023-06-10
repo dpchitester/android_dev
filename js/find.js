127deb284ace4cd8f896d5690016d27e314be0a0
Donald Chitester <dpchitester@gmail.com>
2023-06-10 09:27:27 -0600

    pybak

---
+++ js/find.js
@@ -0,0 +1,18 @@
+const { spawn } = require('child_process');
+
+// const child = spawn('pwd');
+const child = spawn('find', ['.', '-type', 'f']);
+
+child.on('exit', function (code, signal) {
+  console.log('child process exited with ' +
+              `code ${code} and signal ${signal}`);
+});
+
+child.stdout.on('data', (data) => {
+  console.log(`child stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`child stderr: ${data}`);
+});
+
---
+++ js/pwd.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+
+const child = spawn('pwd');
+
+child.on('exit', function (code, signal) {
+  console.log('child process exited with ' +
+              `code ${code} and signal ${signal}`);
+});
+
+child.stdout.on('data', (data) => {
+  console.log(`child stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`child stderr: ${data}`);
+});
+
