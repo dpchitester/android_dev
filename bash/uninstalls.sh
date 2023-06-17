dcb29e0aeb85aa4909b981d0457ccd3f6db54ae3
Donald Chitester <dpchitester@gmail.com>
2023-06-16 20:16:19 -0600

    pybak

--- bash/uninstalls.sh
+++ bash/uninstalls.sh
@@ -2,13 +2,20 @@
 
 
 # pkg uninstall git-crypt
+# pkg uninstall nodejs
 # pkg uninstall openssl-1.1
 # pkg uninstall rclone
 # pkg uninstall ruff
 # pkg uninstall which
 # pip uninstall ----------
+ pip uninstall appdirs
+ pip uninstall baron
 # pip uninstall pip
+ pip uninstall py2many
+ pip uninstall redbaron
+ pip uninstall rply
 # pip uninstall setuptools
-# pip uninstall six
+ pip uninstall six
 # pip uninstall tarjan
+# pip uninstall toposort
 # pip uninstall xxhash
